55bead1f746d57695d554553a3cf59656e7677c3
Ediberto Lima <ediberto45@gmail.com>
2019-06-03 01:47:55 -0300

    maps

--- App.js
+++ App.js
@@ -1,34 +1,148 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { ScrollView, View, Text, StyleSheet, Dimensions } from 'react-native';
 
 import MapView from 'react-native-maps';
 
-
-
-
 export default class App extends Component {
+  state = {
+    places: [
+      {
+        id: 1,
+        title: 'Igrja de fátima',
+        description: 'igreja...',
+        latitude: -3.752056,
+        longitude: -38.526228,
+      },
+      {
+        id: 2,
+        title: 'pitombeira',
+        description: '...',
+        latitude: -3.742252,
+        longitude: -38.534844,
+      },
+      {
+        id: 3,
+        title: 'shoping benbica',
+        description: 'o melhor da codade',
+        latitude: -3.739041,
+        longitude: -38.540129,
+      }
+    ]
+  };
+
+  _mapReady = () => {
+    this.state.places[0].mark.showCallout();
+  };
+
   render() {
+    const { latitude, longitude } = this.state.places[0];
+
     return (
-      <MapView
-      initialRegion={{
-        latitude: -3.743873,
-        longitude: -38.535846,
-        latitudeDelta:0.0042,
-        longitudeDelta:0.0031,
-      }}
-      style={styles.mapView}
-      />
+      <View style={styles.container}>
+        <MapView
+          ref={map => this.mapView = map}
+          initialRegion={{
+            latitude,
+            longitude,
+            latitudeDelta: 0.0142,
+            longitudeDelta: 0.0131,
+          }}
+          style={styles.mapView}
+          rotateEnabled={false}
+          //scrollEnabled={false}
+          zoomEnabled={false}
+          showsPointsOfInterest={false}
+          showBuildings={false}
+          onMapReady={this._mapReady}
+        >
+          { this.state.places.map(place => (
+            <MapView.Marker
+              ref={mark => place.mark = mark}
+              title={place.title}
+              description={place.description}
+              key={place.id}
+              coordinate={{
+                latitude: place.latitude,
+                longitude: place.longitude,
+              }}
+            />
+          ))}
+        </MapView>
+        <ScrollView
+          style={styles.placesContainer}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          onMomentumScrollEnd={(e) => {
+            const place = (e.nativeEvent.contentOffset.x > 0)
+              ? e.nativeEvent.contentOffset.x / Dimensions.get('window').width
+              : 0;
+
+            const { latitude, longitude, mark } = this.state.places[place];
+
+            this.mapView.animateToCoordinate({
+              latitude,
+              longitude
+            }, 500);
+
+            setTimeout(() => {
+              mark.showCallout();
+            }, 500)
+          }}
+        >
+          { this.state.places.map(place => (
+            <View key={place.id} style={styles.place}>
+              <Text style={styles.title}>{ place.title }</Text>
+              <Text style={styles.description}>{ place.description }</Text>
+            </View>
+          )) }
+        </ScrollView>
+      </View>
     );
   }
 }
 
+const { height, width } = Dimensions.get('window');
+
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
+  },
+
   mapView: {
     position: 'absolute',
     top: 0,
     left: 0,
     bottom: 0,
     right: 0,
-},
+  },
+
+  placesContainer: {
+    width: '100%',
+    maxHeight: 200,
+  },
+
+  place: {
+    width: width - 40,
+    maxHeight: 100,
+    backgroundColor: '#FFF',
+    marginHorizontal: 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    padding: 20,
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    backgroundColor: 'transparent',
+  },
+
+  description: {
+    color: '#999',
+    fontSize: 12,
+    marginTop: 5,
+  },
 });
